3af51733f227dda5d56667a76f6fd211231ecdbe
HuyLe001 <huyma0903@gmail.com>
2025-06-09 10:00:00 +0700

    xong phần DashBoard và Reports

--- SWP391_TEAM5-dev-Huy-/SWP391_TEAM5-dev-Huy-/SWP391_TEAM5-dev-Huy/SWP391_TEAM5-dev-Huy/gender-healthcare-api/src/main/java/com/gha/gender_healthcare_api/repository/ClinicInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gha.gender_healthcare_api.repository;
-
-import com.gha.gender_healthcare_api.entity.ClinicInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClinicInfoRepository extends JpaRepository<ClinicInfo, Long> {
-}
--- SWP391_TEAM5-dev-Huy-/SWP391_TEAM5-dev-Huy-/SWP391_TEAM5-dev-Huy/SWP391_TEAM5-dev-Huy/gender-healthcare-api/src/main/java/com/gha/gender_healthcare_api/repository/ServiceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gha.gender_healthcare_api.repository;
-
-import com.gha.gender_healthcare_api.entity.MedicalService;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRepository extends JpaRepository<MedicalService, Long> {
-}
